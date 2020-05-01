c30fce6f0d472a6132dbb4edd2c79f4832c6bede
Konstantinos Egkarchos <konsnosl@gmail.com>
2020-05-02 00:15:04 +0300

    Added client, thermometer, instructions, id and sms elements on screen.

---
+++ Panda-mic/Assets/Game/Scripts/Client.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour
+{
+    [SerializeField] private Image clientImg;
+    [SerializeField] private Image maskImg;
+    [SerializeField] private Image glovesImg;
+
+    public ClientData ClientData { private set; get; }
+    
+    public void LoadClient(ClientData newClientData)
+    {
+        ClientData = newClientData;
+
+        clientImg.sprite = ClientData.person;
+        maskImg.sprite = ClientData.mask;
+        glovesImg.sprite = ClientData.gloves;
+    }
+}
