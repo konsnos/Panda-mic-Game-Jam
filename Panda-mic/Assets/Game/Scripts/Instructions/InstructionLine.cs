1a4b253f917fa61f1768f6b62fb80a3cbb0ba89a
Konstantinos Egkarchos <konsnosl@gmail.com>
2020-05-02 12:49:27 +0300

    Code refactor. Instructions animation.

---
+++ Panda-mic/Assets/Game/Scripts/Instructions/InstructionLine.cs
@@ -0,0 +1,37 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Game
+{
+    public class InstructionLine : MonoBehaviour
+    {
+        [SerializeField] private InstructionType instructionType;
+        public InstructionType InstructionType
+        {
+            get
+            {
+                return instructionType;
+            }
+        }
+
+        [SerializeField] private TMP_Text text;
+
+        public InstructionClicked OnClickedEvent;
+
+        public void OnClicked()
+        {
+            OnClickedEvent?.Invoke(instructionType);
+        }
+
+        public void UpdateText(string newText)
+        {
+            text.text = newText;
+        }
+    }
+
+    [System.Serializable]
+    public class InstructionClicked : UnityEvent<InstructionType>
+    {
+    }
+}
---
+++ Panda-mic/Assets/Game/Scripts/Instructions/InstructionsPanel.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class InstructionsPanel : MonoBehaviour
+    {
+        [SerializeField] private InstructionLine[] instructions;
+
+        [Header("Animation")]
+        [SerializeField] private Animator animator;
+        private int isOpenParam = Animator.StringToHash("isOpen");
+        private bool isOpen = true;
+
+        private InstructionsConfiguration configuration;
+
+        public void LoadConfiguration(InstructionsConfiguration newConfiguration)
+        {
+            configuration = newConfiguration;
+
+            instructions[(int)InstructionType.temperature].UpdateText($"<={configuration.temperature:N1}°C");
+            instructions[(int)InstructionType.gloves].gameObject.SetActive(configuration.glovesRequired);
+            instructions[(int)InstructionType.mask].gameObject.SetActive(configuration.maskRequired);
+            instructions[(int)InstructionType.symptoms].gameObject.SetActive(configuration.symptoms);
+            instructions[(int)InstructionType.request].gameObject.SetActive(configuration.requestIdRequired);
+            instructions[(int)InstructionType.amount].UpdateText($"<={configuration.amountOfPeopleInside}");
+
+            foreach (InstructionLine item in instructions)
+            {
+                item.OnClickedEvent.AddListener(OnInstructionClicked);
+            }
+        }
+
+        private void OnInstructionClicked(InstructionType arg0)
+        {
+            Debug.LogWarning("Unhandled instruction clicked: " + arg0);
+        }
+
+        public void OpenCloseInstructionsClicked()
+        {
+            isOpen = !isOpen;
+            animator.SetBool(isOpenParam, isOpen);
+        }
+    }
+
+    public enum InstructionType
+    {
+        temperature,
+        gloves,
+        mask,
+        symptoms,
+        request,
+        amount
+    }
+}
